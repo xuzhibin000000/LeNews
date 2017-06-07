28bb65cabdcf8b743349b09cbf843529b5f0f895
cestbon <1>
2017-06-07 21:09:46 +0800

    20170607第7次提交-----已实现极光推送功能，加油！

--- app/src/main/java/com/hr/cestbon/fragment/HomePageNewsFragment.java
+++ app/src/main/java/com/hr/cestbon/fragment/HomePageNewsFragment.java
@@ -8,16 +8,32 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
 import com.hr.cestbon.lenews.R;
+import com.hr.cestbon.model.DatePageResult;
+import com.hr.cestbon.model.News;
+import com.hr.cestbon.utils.Constants;
+import com.loopj.android.image.SmartImageView;
 
+import net.lucode.hackware.magicindicator.MagicIndicator;
+import net.lucode.hackware.magicindicator.ViewPagerHelper;
+import net.lucode.hackware.magicindicator.buildins.circlenavigator.CircleNavigator;
+
+import org.xutils.common.Callback;
 import org.xutils.common.util.DensityUtil;
+import org.xutils.http.RequestParams;
+import org.xutils.image.ImageOptions;
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
@@ -35,7 +51,15 @@ public class HomePageNewsFragment extends Fragment {
     // 视图对象
     @ViewInject(R.id.viewPagerNews)
     private ViewPager viewPager;
+    @ViewInject(R.id.magic_indicator2)
+    private MagicIndicator magicIndicator;
+    @ViewInject(R.id.homeListView)
+    private ListView listView;
 
+    private static final String[] CHANNELS = new String[]{"CUPCAKE", "DONUT", "ECLAIR"};
+    //, "GINGERBREAD", "HONEYCOMB", "ICE_CREAM_SANDWICH", "JELLY_BEAN", "KITKAT", "LOLLIPOP", "M", "NOUGAT"};
+
+    private List<News> newsList = new ArrayList<News>();
 
     @Nullable
     @Override
@@ -50,8 +74,128 @@ public class HomePageNewsFragment extends Fragment {
 
         //给ViewPager添加页面适配器
         viewPager.setAdapter(new PageAdapter());
+
+        initMagicIndicator2();
+
+        getServiceData();
     }
 
+    private void initMagicIndicator2() {
+//        MagicIndicator magicIndicator = (MagicIndicator)findViewById(R.id.magic_indicator2);
+        CircleNavigator circleNavigator = new CircleNavigator(this.getActivity());
+        circleNavigator.setFollowTouch(false);
+        circleNavigator.setCircleCount(CHANNELS.length);
+        circleNavigator.setCircleColor(Color.WHITE);
+        circleNavigator.setCircleClickListener(new CircleNavigator.OnCircleClickListener() {
+            @Override
+            public void onClick(int index) {
+                viewPager.setCurrentItem(index);
+            }
+        });
+        magicIndicator.setNavigator(circleNavigator);
+        ViewPagerHelper.bind(magicIndicator, viewPager);
+    }
+
+    private void getServiceData(){
+        RequestParams params = new RequestParams("http://hiwbs101083.jsp.jspee.com.cn/NewsServlet");
+        // 有搜索框的时候实现
+//        params.addQueryStringParameter("wd", "xUtils");
+        Log.i(Constants.tag, "getServiceData---->");
+        x.http().get(params, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+
+                // 2: Gson 把string转化为model /map
+                Log.i(Constants.tag, "HomePageNewsFragment---->onSuccess-->result: "+result);
+                Gson gson = new Gson();
+//                final List<News> newsList = gson.fromJson(result, new TypeToken<ArrayList<News>>() {
+                //                }.getType());
+                DatePageResult fromJson = gson.fromJson(result, DatePageResult.class);
+                Log.i("jxy", "从后台返回的数据为:" + fromJson);
+
+                newsList = fromJson.getNewsList();
+
+                // 对数据进行赋值(List_Item的数据适配)
+                listView.setAdapter(new HomeListViewAdapter());
+
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+                Log.i(Constants.tag, "getServiceData---->onError");
+                Toast.makeText(x.app(), "onError" + ex.getMessage(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+                Log.i(Constants.tag, "getServiceData---->onCancelled");
+                Toast.makeText(x.app(), "cancelled", Toast.LENGTH_LONG).show();
+            }
+
+            @Override  // 只要请求完毕,无论成功还是失败,此方法都会执行
+            public void onFinished() {
+                // 下拉刷新完毕
+                Log.i(Constants.tag, "getServiceData---->onFinished");
+//                listView.endPulldownToRefresh();
+            }
+
+        });
+    }
+
+    private class HomeListViewAdapter extends BaseAdapter{
+        @Override
+        public int getCount() {
+            return newsList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return newsList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            News obj = (News) getItem(position);
+            Log.i(Constants.tag, "getView---->News:"+obj);
+            View view = null;
+            if (convertView != null) {
+                view = convertView;
+            } else {
+                view = View.inflate(HomePageNewsFragment.this.getActivity(), R.layout.home_page_listitem, null);
+            }
+            TextView title = (TextView)view.findViewById(R.id.home_title);
+            Log.i(Constants.tag, "getView---->TextView:"+title);
+            if(title!=null){
+                title.setText(obj.getTitle());
+            }
+
+            SmartImageView iv = (SmartImageView) view.findViewById(R.id.home_iv);
+            //通过XUTLIS3下载图片
+            ImageOptions options = new ImageOptions.Builder()
+                    // 是否忽略GIF格式的图片
+                    .setIgnoreGif(false)
+                    // 图片缩放模式
+                    .setImageScaleType(ImageView.ScaleType.FIT_CENTER)
+                    // 下载中显示的图片
+                    .setLoadingDrawableId(R.drawable.time_loading)
+                    // 支持缓存操作,默认true
+                    .setUseMemCache(true)
+                    // 下载失败显示的图片
+                    .setFailureDrawableId(R.drawable.time_fail)
+                    // 得到ImageOptions对象
+                    .build();
+
+            // 通过xutils3 下载图片,并且支持缓存功能
+            x.image().bind(iv, obj.getImgUrl(), options);
+
+            return view;
+        }
+    }
 
     private class PageAdapter extends PagerAdapter {
         private List<View> vList = new ArrayList<View>();
@@ -95,54 +239,6 @@ public class HomePageNewsFragment extends Fragment {
 //        }
     }
 
-//    @Override
-//    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-//        super.onViewCreated(view, savedInstanceState);
-//
-//        Log.i(Constants.tag,"onViewCreated----->");
-//
-//        viewPager = (ViewPager) this.getActivity().findViewById(R.id.vp_view);
-//
-//        tabLayout = (TabLayout) this.getActivity().findViewById(R.id.tabLayout);
-//        // 新建适配器
-//        tabAdaper = new TabAdaper(this.getActivity().getSupportFragmentManager());
-//        // 设置适配器
-//        viewPager.setAdapter(tabAdaper);
-//        // 直接绑定viewpager，消除了以前的需要设置监听器的繁杂工作
-//        tabLayout.setupWithViewPager(viewPager);
-//
-//    }
-//
-//    // fragment的适配器类
-//    class TabAdaper extends FragmentPagerAdapter {
-//
-//        List<Fragment> fragmentList = new ArrayList<>();
-//        // 标题数组
-//        String[] titles = {"要闻", "直播", "国际", "体育",  "军事"};  //, "财经","军事", "财经", "社会"
-//
-//        public TabAdaper(FragmentManager fm) {
-//            super(fm);
-//            fragmentList.add(new KeyNewsFragment());
-//            fragmentList.add(new LiveRadioFragment());
-//            fragmentList.add(new SportsFragment());
-//            fragmentList.add(new InterFragment());
-//            fragmentList.add(new FinanceFragment());
-//        }
-//
-//        @Override
-//        public CharSequence getPageTitle(int position) {
-//            return titles[position];
-//        }
-//
-//        @Override
-//        public Fragment getItem(int position) {
-//            return fragmentList.get(position);
-//        }
-//
-//        @Override
-//        public int getCount() {
-//            return fragmentList.size();
-//        }
-//    }
+
 
 }
